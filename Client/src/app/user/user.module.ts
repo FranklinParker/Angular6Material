3273cef5691adcf253277f1463d50bea30331ef3
Franklin Parker <fparker@deliveryman.com>
2018-05-08 21:01:23 -0400

    login form

--- Client/src/app/user/user.module.ts
+++ Client/src/app/user/user.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+import {MaterialModule} from "../material.module";
+import {SharedModule} from "../shared/shared.module";
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [
     LoginComponent,
