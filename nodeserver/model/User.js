00159d9c223bf9d966897642404154e9ae82d95a
Franklin Parker <fparker@deliveryman.com>
2018-05-09 17:38:38 -0400

    bcrypt password

--- nodeserver/model/User.js
+++ nodeserver/model/User.js
@@ -1,17 +1,42 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
 const mongooseUniqueValidator = require('mongoose-unique-validator');
 const Schema = mongoose.Schema;
 
 
 const UserSchema = new Schema({
-    firstName:{type:String,required:true},
-    lastName:{type:String,required:true},
-    password:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    roles:[{ type:String}]
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  password: {type: String, required: true},
+  email: {type: String, required: true, unique: true},
+  roles: [{type: String}]
+});
+
+UserSchema.pre('save', function (next) {
+  var user = this;
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        next(new Error('error gen salt'));
+      } else {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          if (err) {
+            next(new Error('error gen salt'));
+          }
+          user.password = hash;
+          next();
+        });
+      }
+    });
+  } else {
+    next();
+  }
 });
 
 UserSchema.plugin(mongooseUniqueValidator);
+
+
 const User = mongoose.model('User', UserSchema);
 
 
