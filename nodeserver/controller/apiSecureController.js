47a933591fd8805aab2babe40462a04a22c92f9e
Franklin Parker <fparker@deliveryman.com>
2018-05-09 13:20:07 -0400

    reformat server code

--- nodeserver/controller/apiSecureController.js
+++ nodeserver/controller/apiSecureController.js
@@ -1,14 +1,20 @@
 const isLoggedIn = true;
 
+/**
+ * secure api routes
+ *
+ * @param req
+ * @param response
+ * @param next
+ */
 function secureApiRoutes(req, response, next) {
-    if(isLoggedIn){
-        next();
-    } else {
-        response.status(200).json({
-            accessNotAllowed: "Not allowed"
-        });
-    }
-
-
+  if (isLoggedIn) {
+    next();
+  } else {
+    response.status(200).json({
+      accessNotAllowed: "Not allowed"
+    });
+  }
 }
+
 module.exports.secureApiRoutes = secureApiRoutes;
--- nodeserver/server.js
+++ nodeserver/server.js
@@ -10,7 +10,7 @@ const databaseDb = require('./database/mongooseDb');
 
 var index = require('./routes/index');
 //var users = require('./routes/users');
-const  routeConfigurer = require('./routes/routes');
+const routeConfigurer = require('./routes/routes');
 
 
 var app = express()
@@ -18,10 +18,10 @@ var app = express()
 var app = express();
 
 app.use(session({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: true
-}))
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,46 +31,44 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 
-
-//app.use('/', index);
 routeConfigurer.configureUnprotectedRoutes(app);
 routeConfigurer.configureProtectedRoutes(app);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 databaseDb.connectToDb();
 
 
 var port = process.env.PORT || 3000
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("To view your app, open this link in your browser: http://localhost:" + port);
 });
 
