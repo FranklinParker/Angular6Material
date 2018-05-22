04ece409e0a93d286f38948594eba57020121d59
Franklin Parker <fparker@deliveryman.com>
2018-05-22 17:57:24 +0900

    contact model

--- Client/src/app/contacts/components/conact-add/conact-add.component.ts
+++ Client/src/app/contacts/components/conact-add/conact-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Contact} from "../../models/contact";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-conact-add',
@@ -20,6 +21,10 @@ export class ConactAddComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(contactForm:NgForm){
+    console.log('contact', this.contact);
   }
 
 }
--- nodeserver/model/Contacts.js
+++ nodeserver/model/Contacts.js
@@ -1,18 +1,25 @@
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ContactSchema = new Schema({
-    firstName: {
-        type: String,
-        required: [true, ' First Name is required.']
-    },
-    lastName: {
-        type: String,
-        required: [true, ' Last Name.']
-    },
-    apartmentNumber: String,
-    cellPhone: String
+  firstName: {
+    type: String,
+    required: [true, ' First Name is required.']
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last Name.']
+  },
+  apartmentNumber: String,
+  phones: [
+    {
+      type: String,
+      number: String
+    }
+  ],
+  email: String,
+  description: String,
+
 });
 
 const Contact = mongoose.model('Contact', ContactSchema);
